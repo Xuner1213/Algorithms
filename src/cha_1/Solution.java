8f31c49dc856952bee63901f4e95cc6f367f6ed0
meixun <1531002208@qq.com>
2019-10-25 14:49:11 +0800

    409最大回文串

--- src/cha_1/Solution.java
+++ src/cha_1/Solution.java
@@ -830,6 +830,22 @@ class Solution {
         return s;
     }
 
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        for (char c : s.toCharArray()) {
+            count[c]++;
+        }
+
+        int ans = 0;
+        for (int v : count) {
+            ans += v / 2 * 2;
+            if(v % 2 == 1 && ans % 2 == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         String res = toHex(35);
         System.out.println(res);
