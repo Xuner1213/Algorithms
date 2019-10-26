de39fa05cccc47908efe6a94e50e051500818fb4
Xuner1213 <1531002208@qq.com>
2019-10-26 18:45:50 +0800

    415

--- src/cha_1/Solution.java
+++ src/cha_1/Solution.java
@@ -915,7 +915,24 @@ class Solution {
         }
     }
 
+    public String addStrings(String num1, String num2) {
+        StringBuilder stringBuilder = new StringBuilder("");
+        int i = num1.length() - 1, j = num2.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int n1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int n2 = j >= 0 ? num2.charAt(j) - '0' : 0;
 
+            int tmp = n1 + n2 + carry;
+            carry = tmp / 10;
+            stringBuilder.append(tmp % 10);
+            i--;
+            j--;
+        }
+        if (carry == 1){
+            stringBuilder.append(1);
+        }
+        return stringBuilder.reverse().toString();
+    }
 
 
     public static void main(String[] args) {
