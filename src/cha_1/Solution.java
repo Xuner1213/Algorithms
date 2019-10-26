d2492513cdc40b82ab88eecef44ad409da61ed4f
Xuner1213 <1531002208@qq.com>
2019-10-26 23:17:33 +0800

    437

--- src/cha_1/Solution.java
+++ src/cha_1/Solution.java
@@ -935,6 +935,97 @@ class Solution {
     }
 
 
+//    public List<List<Integer>> levelOrder(Node root) {
+//        List<List<Integer>> res = new ArrayList<List<Integer>>();
+//        if (root == null) {
+//            return res;
+//        }
+//
+//        Queue<Node> queue = new LinkedList<Node>();
+//        queue.add(root);
+//        while (!queue.isEmpty()) {
+//            int count = queue.size();
+//            List<Integer> list = new ArrayList<Integer>();
+//            while (count-- > 0) {
+//                Node cur = queue.poll();
+//                list.add(cur.val);
+//                for (Node node : cur.children) {
+//                    if (node != null) {
+//                        queue.add(node);
+//                    }
+//                }
+//            }
+//            res.add(list);
+//        }
+//
+//        return res;
+//    }
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return res;
+        }
+        helper(root, 0, res);
+        return res;
+    }
+
+    private void helper(Node root, int depth, List<List<Integer>> res) {
+        if (root == null) {
+            return;
+        }
+
+        if (depth + 1 > res.size()) {
+            res.add(new ArrayList<Integer>());
+        }
+
+        res.get(depth).add(root.val);
+
+        for (Node node : root.children) {
+            if (node != null) {
+                helper(node, depth + 1, res);
+            }
+        }
+    }
+
+    public int countSegments(String s) {
+        int segmentCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') {
+                segmentCount++;
+            }
+        }
+
+        return segmentCount;
+    }
+
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+
+        return paths(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    }
+
+    private int paths(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+
+        int res = 0;
+
+        if (root.val == sum) {
+            res += 1;
+        }
+
+        res += paths(root.left, sum - root.val);
+        res += paths(root.right, sum - root.val);
+
+        return res;
+    }
+
+
     public static void main(String[] args) {
         String res = toHex(35);
         System.out.println(res);
@@ -1104,6 +1195,18 @@ class MyQueue {
         return stackPush.isEmpty() && stackPop.isEmpty();
     }
 }
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
 
 
 
