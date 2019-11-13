4c198c26ad5ce18b9d4fe8c75872553ce0626451
meixun <1531002208@qq.com>
2019-11-13 23:35:53 +0800

    551. 学生出勤记录 I

--- src/cha_1/Solution.java
+++ src/cha_1/Solution.java
@@ -1500,6 +1500,18 @@ class Solution {
         return ans - 1;
     }
 
+    public boolean checkRecord(String s) {
+        int countA = 0;
+        for(int i = 0; i < s.length() && countA < 2; i++) {
+            if (s.charAt(i) == 'A') {
+                countA++;
+            }
+            if (i <= s.length() - 3 && s.charAt(i) == 'L' && s.charAt(i + 1) == 'L' && s.charAt(i + 2) == 'L') {
+                return false;
+            }
+        }
+        return countA < 2;
+    }
 
 
     public static void main(String[] args) {
