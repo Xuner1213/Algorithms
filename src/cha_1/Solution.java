af849c115ef4c1e40e76e5913ed98e3ee09448df
meixun <1531002208@qq.com>
2019-12-04 19:55:45 +0800

    561. 数组拆分 I

--- src/cha_1/Solution.java
+++ src/cha_1/Solution.java
@@ -1580,6 +1580,15 @@ class Solution {
         }
     }
 
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         int a = 5;
         System.out.println(findComplement(a));
